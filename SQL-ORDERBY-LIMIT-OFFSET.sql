d223083b26c37a9f16c9aaa218cb9070366a6d7c
ceyy <115035809+ceyhunkayacann@users.noreply.github.com>
2022-12-22 04:30:23 +0300

    Add files via upload

---
+++ SQL-ORDERBY-LIMIT-OFFSET.sql
@@ -0,0 +1,16 @@
+SELECT title FROM film
+WHERE title ILIKE '%n'
+ORDER BY length 
+LIMIT 5;
+
+SELECT title FROM film
+WHERE title ILIKE '%n'
+ORDER BY length DESC
+OFFSET 5
+LIMIT 5;
+
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4
+
